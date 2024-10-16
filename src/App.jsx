48de18ff771a3fb9b112a2b0bf7f3664aacebbbf
Aria Radmehr <radmehr.krash@gmail.com>
2024-10-17 00:12:29 +0200

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import SubscriptionCards from './pages/shop';
 import LearningPathsPage from './pages/LearningPath';
 import LearningPathDetail from './pages/LearningPathDetail';
 
+
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -40,36 +41,35 @@ const App = () => {
 
   return (
     <Router>
-      <Layout
-        isLoggedIn={isLoggedIn}
-        isDarkTheme={isDarkTheme}
-        toggleTheme={toggleTheme}
-        handleLogout={handleLogout}
-        changeLanguage={changeLanguage} // Pass it here
-        currentLanguage={i18n.language} // 
-              >
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/course/:id" element={<CourseLandingPage />} />
-          <Route path="/courses/:courseId/lessons/:lessonId" element={<LessonPage />} />
-          <Route path="/profile" element={<UserProfilePage />} />
-          <Route path="/quiz" element={<QuizComponent />} />
-          <Route path="/shop" element={<SubscriptionCards />} />
-          <Route path="/ask" element={<RequestPage />} />
-          <Route path="/cha" element={<CharacterIntroPage />} />
-          <Route path="/cha1" element={<FuturisticFamilyPage />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/my-courses" element={<MyCourses />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/survey" element={<Survey />} />
-          <Route path="/learning-paths" element={<LearningPathsPage />} />
-          <Route path="/learning-paths/:id" element={<LearningPathDetail />} />
-
-        </Routes>
-      </Layout>
+      <div dir={i18n.language === 'fa' ? 'rtl' : 'ltr'}>
+        <Layout
+          isLoggedIn={isLoggedIn}
+          isDarkTheme={isDarkTheme}
+          toggleTheme={toggleTheme}
+          handleLogout={handleLogout}
+          changeLanguage={changeLanguage} // Pass it here
+          currentLanguage={i18n.language} // 
+        >
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/course/:id" element={<CourseLandingPage />} />
+            <Route path="/courses/:courseId/lessons/:lessonId" element={<LessonPage />} />
+            <Route path="/profile" element={<UserProfilePage />} />
+            <Route path="/quiz" element={<QuizComponent />} />
+            <Route path="/shop" element={<SubscriptionCards />} />
+            <Route path="/ask" element={<RequestPage />} />
+            <Route path="/cha" element={<CharacterIntroPage />} />
+            <Route path="/cha1" element={<FuturisticFamilyPage />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/my-courses" element={<MyCourses />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/survey" element={<Survey />} />
+          </Routes>
+        </Layout>
+      </div>
     </Router>
   );
 };
